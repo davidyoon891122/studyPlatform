4074e19862f0924e1aed1a7e14e46e1aef79da55
davidyoon <qqpo12@naver.com>
2021-07-24 21:00:45 +0900

    Initial Commit

---
+++ studyPlatform/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  studyPlatform
+//
+//  Created by David Yoon on 2021/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
